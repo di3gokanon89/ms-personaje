ccd756cdf4483be797d94ac927e017e015cf9a2b
di3gokanon89 <diego.paniagua.kanon89@gmail.com>
2021-07-21 08:41:17 -0500

    Proyecto ms-personaje: versión inicial del microservicio

---
+++ src/main/java/com/devpredator/MsPersonajeApplication.java
@@ -0,0 +1,13 @@
+package com.devpredator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPersonajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPersonajeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpredator/MsPersonajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpredator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPersonajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
